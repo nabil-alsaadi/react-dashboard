40648dcd33675112fe38ba87b8798eb80dfe80e2
nabil alsaadi <nabil.biloo.alsaadi95@gmail.com>
2025-02-25 19:40:17 +0400

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import SidebarLayout from "./components/SidebarLayout";
 import Users from "./pages/Users";
 import Products from "./pages/Products";
@@ -11,7 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/users" element={<Users />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/" element={<Users />} /> {/* Default page */}
+          <Route path="/" element={<Navigate to="/users" replace />} />
         </Routes>
       </SidebarLayout>
     </Router>
